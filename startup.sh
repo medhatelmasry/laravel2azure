603c7306beb6e9234836f9e13b51dc477481f532
Medhat Elmasry <medhat@elmasry.ca>
2025-03-20 14:26:43 -0700

    startup

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /home/site/wwwroot/storage/framework/views
+mkdir -p /home/site/wwwroot/bootstrap/cache
+chmod -R 777 /home/site/wwwroot/storage
+chmod -R 777 /home/site/wwwroot/bootstrap/cache
