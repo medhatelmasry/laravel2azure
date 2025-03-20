b60b1e5875c01f9810a19d58f0bba45d9871a752
Medhat Elmasry <medhat@elmasry.ca>
2025-03-20 16:30:55 -0700

    upload startup.sh

--- startup.sh
+++ startup.sh
@@ -3,3 +3,8 @@ mkdir -p /home/site/wwwroot/storage/framework/views
 mkdir -p /home/site/wwwroot/bootstrap/cache
 chmod -R 777 /home/site/wwwroot/storage
 chmod -R 777 /home/site/wwwroot/bootstrap/cache
+php /home/site/wwwroot/artisan cache:clear
+php /home/site/wwwroot/artisan config:clear
+php /home/site/wwwroot/artisan config:cache
+php /home/site/wwwroot/artisan route:clear
+php /home/site/wwwroot/artisan view:clear
