fa3dbce6ee97643c77c30cd964b72b91920db628
Medhat Elmasry <medhat@elmasry.ca>
2025-03-20 19:47:17 -0700

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -8,5 +8,4 @@ php /home/site/wwwroot/artisan config:clear
 php /home/site/wwwroot/artisan config:cache
 php /home/site/wwwroot/artisan route:clear
 php /home/site/wwwroot/artisan view:clear
-cp /home/site/wwwroot/default /etc/nginx/sites-available/default
-service nginx reload
+cp /home/site/wwwroot/default /etc/nginx/sites-available/default && service nginx reload
