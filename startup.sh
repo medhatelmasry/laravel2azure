97c9890650cb2f59111b6fd260febf11f4b626da
Medhat Elmasry <medhat@elmasry.ca>
2025-03-20 19:57:14 -0700

    updated default and startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 mkdir -p /home/site/wwwroot/storage/framework/views
 mkdir -p /home/site/wwwroot/bootstrap/cache
-chmod -R 777 /home/site/wwwroot/storage
-chmod -R 777 /home/site/wwwroot/bootstrap/cache
+chmod -R 775 /home/site/wwwroot/storage
+chmod -R 775 /home/site/wwwroot/bootstrap/cache
+chown -R www-data:www-data /home/site/wwwroot/storage
+chown -R www-data:www-data /home/site/wwwroot/bootstrap/cache
 php /home/site/wwwroot/artisan cache:clear
 php /home/site/wwwroot/artisan config:clear
 php /home/site/wwwroot/artisan config:cache
 php /home/site/wwwroot/artisan route:clear
 php /home/site/wwwroot/artisan view:clear
-cp /home/site/wwwroot/default /etc/nginx/sites-available/default && service nginx reload
+cp /home/site/wwwroot/default /etc/nginx/sites-available/default
+ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+service nginx reload
